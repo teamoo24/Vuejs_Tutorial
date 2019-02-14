3ec1f448606115b2425a4e04997c0e9cabc59dd2
Han <601107w@gmail.com>
2019-02-14 15:18:59 +0900

    v-onでイベントを設定

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
 (function(){
 	'use strict'
 
-	// two 
+	// ここでVueを定義 
 	var vm = new Vue({
 		el: '#app',
 		data: {
+			newItem : '',
 			todos: [
 				'task 1',
 				'task 2',
 				'task 3'
 			]
+		},
+		methods: {
+			// ここでaddItemを自邸
+			addItem: function(){
+				// submitを押した時に再読み込みを防ぐ
+				// e.preventDefault();
+				// todos配列にnewItemの中身を追加
+				this.todos.push(this.newItem);
+				// pushした後にnewItemを空文字にする
+				this.newItem = '';
+			}
 		}
 	});
 })();
